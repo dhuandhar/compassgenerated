614057e92219ea90a6b87bdbc0dca24aba632b81
dhuandhar <31077500+dhuandhar@users.noreply.github.com>
2024-09-20 19:31:16 +0530

    Update genkit.config.ts

--- src/lib/genkit/genkit.config.ts
+++ src/lib/genkit/genkit.config.ts
@@ -1,51 +1,10 @@
-/**
- * Copyright 2024 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import { configureGenkit } from '@genkit-ai/core';
-import { dotprompt } from '@genkit-ai/dotprompt';
-import { firebase } from '@genkit-ai/firebase';
-import { vertexAI } from '@genkit-ai/vertexai';
-
-// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // ...
-  projectId: 'REPLACE_WITH_YOUR_PROJECT_ID',
-  // ...
+  apiKey: "AIzaSyCAonPzMTy5zDM7WxD4XWjcgCX_YoC5q-A",
+  authDomain: "compassgenerated.firebaseapp.com",
+  projectId: "compassgenerated",
+  storageBucket: "compassgenerated.appspot.com",
+  messagingSenderId: "433139596624",
+  appId: "1:433139596624:web:94e408dc07cd5c6f718055",
+  measurementId: "G-N44BC2SYV0"
 };
-
-export const getProjectId = () => {
-  return firebaseConfig.projectId;
-};
-
-export const config = configureGenkit({
-  plugins: [
-    firebase({
-      projectId: getProjectId(),
-      traceStore: { collection: 'traceStore' },
-      flowStateStore: { collection: 'flowTraceStore' },
-    }),
-    vertexAI({
-      projectId: getProjectId(),
-      location: 'us-central1',
-    }),
-    dotprompt(),
-  ],
-  flowStateStore: 'firebase',
-  traceStore: 'firebase',
-  enableTracingAndMetrics: true,
-  logLevel: 'debug',
-  // promptDir: join(process.cwd(), 'public', 'prompts'), -- not working
-});
